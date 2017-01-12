ea1b496e54ab8f3f34040333f70f9514d157d933
mukul <mukulraw199517@gmail.com>
2017-01-12 19:11:48 +0530

    asd

--- app/src/main/java/com/example/solomon/soloapp/Login.java
+++ app/src/main/java/com/example/solomon/soloapp/Login.java
@@ -1,13 +1,130 @@
 package com.example.solomon.soloapp;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+
+    private SignInButton google_signin;
+    private GoogleApiClient mGoogleApiClient;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        google_signin = (SignInButton) findViewById(R.id.sign_in_button);
+
+        google_signin.setSize(SignInButton.SIZE_STANDARD);
+
+
+        if(checkPlayServices())
+        {
+
+            buildGoogleApiClient();
+
+            google_signin.setOnClickListener(this);
+
+        }
+
+
+    }
+
+
+    private boolean checkPlayServices() {
+
+        int checkGooglePlayServices = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        //Log.i("DEBUG_TAG",
+        // "checkGooglePlayServicesAvailable, connectionStatusCode="
+        // + checkGooglePlayServices);
+
+        if (GooglePlayServicesUtil.isUserRecoverableError(checkGooglePlayServices)) {
+            showGooglePlayServicesAvailabilityErrorDialog(checkGooglePlayServices);
+            return false;
+        }
+
+
+        return true;
+
+    }
+
+
+
+    private void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        this.runOnUiThread(new Runnable() {
+            public void run() {
+                final Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode,Login.this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST);
+                if (dialog == null) {
+                    //Log.e("DEBUG_TAG",
+                    //"couldn't get GooglePlayServicesUtil.getErrorDialog");
+                    Toast.makeText(getBaseContext(),
+                            "incompatible version of Google Play Services",
+                            Toast.LENGTH_LONG).show();
+
+                    Button button = (Button)findViewById(R.id.button);
+                    button.setVisibility(View.GONE);
+
+                    dialog.show();
+                }
+                //this was wrong here -->dialog.show();
+            }
+        });
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .enableAutoManage(this , this)
+        .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+        .build();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
